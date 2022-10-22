b40106acab9c1b412a10054549df393aeb96f635
Raines Demint <raines@qaladder.com>
2022-10-22 09:12:15 -0700

    Fixed slug on blog entries

--- pages/blog/[slug].js
+++ pages/blog/[slug].js
@@ -30,8 +30,8 @@ export default function BlogPage({mdxSource, frontMatter}) {
     return (
         <Container>
             <div className="flex flex-col items-center max-w-7xl">
-            <h1 className="text-4xl max-w-3xl text-center pt-8 pb-4 border-b-2 border-rose-200">{frontMatter.title}</h1>
-            <div className="prose">
+            <h1 className="text-4xl max-w-2xl text-center pt-8 pb-4 border-b-2 border-rose-200">{frontMatter.title}</h1>
+            <div className="prose pt-12">
                 <MDXRemote {...mdxSource} />
                 </div>
             </div>

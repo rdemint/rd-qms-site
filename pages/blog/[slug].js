8db49def0f8245e7ccef8e1495013cf64fef262c
Raines Demint <raines@qaladder.com>
2022-10-21 17:54:22 -0700

    Basic blog page set up.

--- pages/blog/[slug].js
+++ pages/blog/[slug].js
@@ -2,7 +2,7 @@ import matter from "gray-matter";
 import { serialize } from "next-mdx-remote/serialize";
 import { MDXRemote } from "next-mdx-remote";
 import { getFileData, getPaths } from "@/lib/mdxfiles";
-import Header from '@/components/Header'
+import Container from '@/components/Container'
 
 export async function getStaticPaths() {
     const paths = getPaths('blog')
@@ -28,11 +28,13 @@ export async function getStaticProps(context) {
 
 export default function BlogPage({mdxSource, frontMatter}) {
     return (
-        <div>
-            <h1 className="text-2xl">{frontMatter.title}</h1>
+        <Container>
+            <div className="flex flex-col items-center max-w-7xl">
+            <h1 className="text-4xl max-w-3xl text-center pt-8 pb-4 border-b-2 border-rose-200">{frontMatter.title}</h1>
             <div className="prose">
                 <MDXRemote {...mdxSource} />
+                </div>
             </div>
-        </div>
+        </Container>
     )
 }

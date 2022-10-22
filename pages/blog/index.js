4319498c8cc0739bf43bb949492c6ba351a63195
Raines Demint <raines@qaladder.com>
2022-10-22 07:29:39 -0700

    styling updates on post list

--- pages/blog/index.js
+++ pages/blog/index.js
@@ -16,21 +16,23 @@ export default function BlogHome({posts}) {
 
         <Container>
             <h2 className="text-4xl tracking-tight font-medium pt-8 pb-4">All posts</h2>
-            <p className="text-gray-700">Where I write articles on medtech, quality management systems, software.</p>
+            <p className="text-gray-700">Where I write on medtech, quality management systems, and software.</p>
             <p className="text-gray-700">Looking for bite-sized info on specific topics?  Head over to the <span className="text-rose-500"><Link href="/docs">Docs.</Link></span></p>
             <ul role="list" className="pt-6 space-y-5 max-w-3xl mx-auto">
                 {posts.map(
                     (post) => (
-                        <li className="border-l-2 border-rose-200 px-6 py-4" key={post.title}>
+                        <li className="border-l-2 border-rose-200 px-6 py-4 hover:bg-gray-50 rounded" key={post.title}>
                             <Link href={`/blog/${post.slug}`}>
-                                <a className="hover:text-rose-600">{post.title}</a>
+                                <a className="hover:text-gray-800">
+                                    {post.title}
+                                    <p className="pt-1 text-sm text-gray-600">{post.summary}</p>
+                                    <div class="flex space-x-2">
+                                    {post.tags.map(
+                                        (tag) => (<div className="text-sm text-gray-700" key={tag}>{tag}</div>)
+                                        )}
+                                    </div>
+                                </a>
                             </Link>
-                            <p className="pt-1 text-sm text-gray-600">{post.summary}</p>
-                            <div class="flex space-x-2">
-                            {post.tags.map(
-                                (tag) => (<div className="text-sm text-gray-700" key={tag}>{tag}</div>)
-                                )}
-                            </div>
 </li>
                     )
                 )}

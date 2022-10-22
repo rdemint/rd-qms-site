e9c0be4041c24620911c5da8259a44418cbcc6fc
Raines Demint <raines@qaladder.com>
2022-10-21 18:56:06 -0700

    Post list view has styling.

--- pages/blog/index.js
+++ pages/blog/index.js
@@ -1,10 +1,6 @@
 import { getFiles } from '@/lib/mdxfiles'
-
-import matter from "gray-matter";
-import { serialize } from "next-mdx-remote/serialize";
-import { MDXRemote } from "next-mdx-remote";
-import { getFileData, getPaths } from "@/lib/mdxfiles";
 import Container from '@/components/Container'
+import Link from 'next/link'
 
 export async function getStaticProps() {
     const posts = getFiles('blog')
@@ -19,12 +15,23 @@ export default function BlogHome({posts}) {
     return (
 
         <Container>
-            <h2 className="text-4xl tracking-tight font-medium pt-8 pb-4">All blog articles</h2>
-            <p className="text-gray-700">Because writing is cheaper than therapy...</p>
-            <ul role="list" className="divide-y divide-gray-200 space-y-3 max-w-3xl mx-auto">
+            <h2 className="text-4xl tracking-tight font-medium pt-8 pb-4">All posts</h2>
+            <p className="text-gray-700">Where I write articles on medtech, quality management systems, software.</p>
+            <p className="text-gray-700">Looking for bite-sized info on specific topics?  Head over to the <span className="text-rose-500"><Link href="/docs">Docs.</Link></span></p>
+            <ul role="list" className="pt-6 space-y-5 max-w-3xl mx-auto">
                 {posts.map(
                     (post) => (
-                        <li className="px-6 py-4" key={post.title}>{post.title}</li>
+                        <li className="border-l-2 border-rose-200 px-6 py-4" key={post.title}>
+                            <Link href={`/blog/${post.slug}`}>
+                                <a className="hover:text-rose-600">{post.title}</a>
+                            </Link>
+                            <p className="pt-1 text-sm text-gray-600">{post.summary}</p>
+                            <div class="flex space-x-2">
+                            {post.tags.map(
+                                (tag) => (<div className="text-sm text-gray-700" key={tag}>{tag}</div>)
+                                )}
+                            </div>
+</li>
                     )
                 )}
             </ul>

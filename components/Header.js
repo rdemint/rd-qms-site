89c248fe1caee59f1da81ce5ce9b535527ccc6e2
Raines Demint <raines@qaladder.com>
2022-10-24 20:57:22 -0700

    Prototyping the landing page.

--- components/Header.js
+++ components/Header.js
@@ -4,7 +4,9 @@ export default function Header() {
 
 return(
     <div className="flex items-center justify-between border-b-2 border-gray-100 p-6">
-        <h2>Raines Demint</h2>
+        <Link href="/#">
+            <a>Raines Demint</a>
+        </Link>
         <section className="flex space-x-5">
             <Link href="/blog" className="rounded px-3 py-1 text-gray-600 hover:text-gray-800 hover:bg-gray-100">Blog</Link>
             <Link href="/about" className="rounded px-3 py-1 text-gray-600 hover:text-gray-800 hover:bg-gray-100">About</Link>
--- pages/docs/index.js
+++ pages/docs/index.js
@@ -5,7 +5,8 @@ export default function DocsPage() {
     return (
         <SidebarLayout>
                 <div className="flex flex-col max-w-7xl px-4">
-                    <h2 className="text-2xl">Welcome to the docs</h2>
+                
+                <h2 className="text-2xl">Welcome to the docs</h2>
                     <p>A place for guides and bite-sized learning on medical device quality management.</p>
                 </div>
             </SidebarLayout>

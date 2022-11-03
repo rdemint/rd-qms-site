f33bc82e6174511c48c1ecc5a184d78376e07a1e
Raines Demint <raines@qaladder.com>
2022-11-03 12:16:00 -0700

    Refined the post index list view with extra info.

--- pages/blog/index.js
+++ pages/blog/index.js
@@ -17,20 +17,22 @@ export default function BlogHome({posts}) {
         <Container>
             <h2 className="text-4xl tracking-tight font-medium pt-8 pb-4">All posts</h2>
             <p className="text-gray-700">Where I write on medtech, quality management systems, and software.</p>
-            <p className="text-gray-700">Looking for bite-sized info on specific topics?  Head over to the <span className="text-rose-500"><Link href="/docs">Docs.</Link></span></p>
+            <p className="text-gray-700">Looking for bite-sized info on specific topics?  Head over to the <span className="text-sky-500"><Link href="/docs">Docs.</Link></span></p>
             <ul role="list" className="pt-6 space-y-5 max-w-3xl mx-auto">
                 {posts.map(
                     (post) => (
-                        <li className="border-l-2 border-rose-200 px-6 py-4 hover:bg-gray-50 rounded" key={post.title}>
+                        <li className="border-l-2 border-sky-200 px-6 py-4 hover:bg-gray-50 rounded" key={post.title}>
                             <Link href={`/blog/${post.slug}`}>
                                 <a className="hover:text-gray-800">
                                     {post.title}
                                     <p className="pt-1 text-sm text-gray-600">{post.summary}</p>
-                                    <div class="flex space-x-2">
+                                    <div class="flex space-x-2 mt-2">
                                     {post.tags.map(
-                                        (tag) => (<div className="text-sm text-gray-700" key={tag}>{tag}</div>)
+                                        (tag) => (<div className="text-xs text-gray-700" key={tag}>'{tag}'</div>)
                                         )}
                                     </div>
+                                    <div className="text-xs text-gray-500 pt-2">{post.date}</div>
+                                    
                                 </a>
                             </Link>
 </li>
--- pages/docs/index.js
+++ pages/docs/index.js
@@ -4,10 +4,12 @@ import SidebarLayout from "../../components/layouts/SidebarLayout";
 export default function DocsPage() {
     return (
         <SidebarLayout>
-                <div className="flex flex-col max-w-7xl px-4">
+                <div className="flex flex-col max-w-7xl px-4 prose">
                 
-                <h2 className="text-2xl">Welcome to the docs</h2>
-                    <p>A place for guides and bite-sized learning on medical device quality management.</p>
+                <h2 className="text-4xl">Welcome to the docs</h2>
+                <p>A place for guides and bite-sized learning on medical device quality management.</p>
+                <p>This space is intended to answer specific, common questions about wide-ranging topics.  The plan is still being built out as far as what to prioritize to write about.</p>
+                <p>Please <span><a href="https://www.linkedin.com/in/raines-demint/">reach out</a></span> with any specific requests for topics and questions.</p>
                 </div>
             </SidebarLayout>
 )

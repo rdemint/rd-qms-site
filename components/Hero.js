fa1698412674051d8a52ab57f56c8af82af98a23
Raines Demint <raines@qaladder.com>
2022-11-03 21:10:49 -0700

    Styling on the SideBarNav

--- components/Hero.js
+++ components/Hero.js
@@ -1,12 +1,19 @@
 export default function Hero() {
     return (
-        <div className="text-center my-12">
-            <h1 className="text-4xl font-bold tracking-tight text-gray-800 sm:text-5xl md:text-6xl">
-                <span className="block xl:inline">Design. Build. Train.</span>{' '}
-                <span className="block text-coral xl:inline">Audit</span>
+        <div className="text-center my-6">
+            <h1 className="text-4xl font-bold tracking-tight text-gray-700 sm:text-5xl md:text-6xl">
+                <span className="block xl:inline">Design</span>
+                <span className="font-light text-primary-300">. </span>
+                <span>Build</span>
+                <span className="font-light text-primary-500">. </span>
+                <span>Train</span>
+                <span className=" font-light text-primary-700">. </span>
+                <span>Audit</span>
+                <span className="font-light text-primary-900  ">. </span>
+                {' '}
             </h1>
             <p className="mx-auto my-3 max-w-md text-base text-gray-500 sm:text-lg md:mt-5 md:max-w-3xl md:text-xl">
-                For a better medical device quality management system. 
+                for better medical device quality management systems. 
             </p>
         </div>
 )
--- pages/index.js
+++ pages/index.js
@@ -31,24 +31,25 @@ export default function Home() {
 
       <main className="flex flex-col max-w-3xl mx-auto">
               <Hero />
-                
-              <div className="flex flex-col space-y-8 my-12 items-center w-full">
+              <div className="flex flex-col space-y-8 my-6 items-center w-full">
               <div className="flex flex-col items-center" >
-                  <a href="/blog" className="rounded-md border border-accent-800 px-4 py-2 w-48 text-center text-accent-900 shadow shadow-accent2-400 hover:shadow-md hover:shadow-accent2-300">The blog</a>
+                  <a href="/blog" className="rounded-md border border-accent-700 px-4 py-2 w-48 text-center text-accent-900 shadow shadow-primary-100 hover:shadow-md">blog</a>
                   <p className="text-gray-500 font-light">Expanded and freeform thoughts</p>
               </div>                                                                    
                                                                                         
                   <div className="flex flex-col items-center">
-                      <a href="/docs" className="rounded-md border border-accent-900 px-4 py-2 w-48 text-center text-accent-900 shadow shadow-accent2-400 hover:shadow-md hover:shadow-accent2-300">The docs</a>
+                      <a href="/docs" className="rounded-md border border-accent-700 px-4 py-2 w-48 text-center text-accent-900 shadow shadow-primary-100 hover:shadow-md">docs</a>
                   <p className="text-gray-500 font-light">Bite-sized learning and topic guides</p>
               </div>
               </div>
-              <p className="text-primary-900 text-center">My mission is to bring teams the tools that get medical device teams to market faster and thrive there.</p>
-              <p className="text-primary-900 text-center">What does your medical device team need to succeed? Reach out on
-                  <span>
-                      <a className="text-sky-800 hover:text-sky-900" href="linkedin.com/raines-demint"> LinkedIn</a>
-                  </span>
-              </p>
+              <div className="my-6">
+                  <p className="text-primary-900 text-center">I build tools for medtech teams so that they can get to market faster and then thrive there.</p>
+                  <p className="text-gray-500 font-light text-center">What does your medical device team need to succeed? Reach out on
+                      <span>
+                          <a className="text-primary-800 hover:text-primary-500" href="linkedin.com/raines-demint"> LinkedIn</a>
+                      </span>
+                      </p>
+              </div>
               <div id="carousel-container"className="flex flex-col align-items mt-12">
                 <div className="relative flex justify-center h-10">
                           <div className={`${current === 0 ? 'opacity-100' : 'opacity-0'} absolute transition-opacity ease-in-out duration-300 text-accent-900 text-md text-center font-light text-lg`}>"{catchphrases[0]}"</div>

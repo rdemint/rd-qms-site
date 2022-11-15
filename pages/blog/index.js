df81f2b529bb2db58761776915500d005da3f1dc
Raines Demint <raines@qaladder.com>
2022-11-14 17:15:50 -0800

    Improved styling on blog list

--- pages/blog/index.js
+++ pages/blog/index.js
@@ -17,23 +17,23 @@ export default function BlogHome({posts}) {
         <Container>
 <div className="max-w-3xl mx-auto">
             <h2 className="text-4xl tracking-tight font-medium pt-8 pb-4">All posts</h2>
-            <p className="text-gray-700">Where I write on medtech, quality management systems, and software.</p>
-            <p className="text-gray-700">Looking for bite-sized info on specific topics?  Head over to the <span className="text-primary-900"><Link href="/docs">Docs.</Link></span></p>
+            <p className="text-slate-700">Where I write on medtech, quality management systems, and software.</p>
+            <p className="text-gray-500">Looking for bite-sized info on specific topics?  Head over to the <span className="text-primary-900"><Link href="/docs">Docs.</Link></span></p>
 
-            <ul role="list" className="pt-6 space-y-5">
+            <ul role="list" className="pt-6 space-y-9">
                 {posts.map(
                     (post) => (
-                        <li className="border-l-2 border-accent-500 px-6 py-4 hover:border-accent-800" key={post.title}>
+                        <li className="text-2xl bg-gradient-to-b from-slate-50 to-white text-slate-800 border-l-2 border-accent-600 px-6 py-6  hover:border-accent-900 shadow" key={post.title}>
                             <Link href={`/blog/${post.slug}`}>
                                 <a className="hover:text-gray-800">
                                     {post.title}
-                                    <p className="pt-1 text-sm text-gray-600">{post.summary}</p>
+                                    <p className="pt-1 text-sm text-slate-700">{post.summary}</p>
                                     <div class="flex space-x-2 mt-2">
                                     {post.tags.map(
-                                        (tag) => (<div className="text-xs text-gray-700" key={tag}>'{tag}'</div>)
+                                        (tag) => (<div className="text-xs text-slate-500" key={tag}>'{tag}'</div>)
                                         )}
                                     </div>
-                                    <div className="text-xs text-gray-500 pt-2">{post.date}</div>
+                                    <div className="text-xs text-slate-500 pt-2">{post.date}</div>
                                     
                                 </a>
                             </Link>

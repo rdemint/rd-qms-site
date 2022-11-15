c8edadd91e598d5f65efe927e6ebfe5f1c1f11ab
Raines Demint <raines@qaladder.com>
2022-11-15 07:37:36 -0800

    Updated favicon

--- pages/_app.js
+++ pages/_app.js
@@ -2,18 +2,23 @@ import '../styles/globals.css'
 import 'instantsearch.css/themes/satellite.css'
 import AppShell from '@/components/AppShell'
 import algoliasearch from 'algoliasearch/lite'
-import { InstantSearch } from 'react-instantsearch-hooks-web';
+import { InstantSearch } from 'react-instantsearch-hooks-web'
+import Head from 'next/head'
 
 const searchClient = algoliasearch(process.env.NEXT_PUBLIC_ALGOLIA_APP_ID, process.env.NEXT_PUBLIC_ALGOLIA_SEARCH_KEY);
 
 function MyApp({ Component, pageProps }) {
     return (
+        <>
+        <Head>
+            <link rel="icon" href="/rd_favicon.png" type="image/png"/>
+        </Head>
         <InstantSearch searchClient={searchClient} indexName="qms_data_index">
         <AppShell>
             <Component {...pageProps} />
         </AppShell>
         </InstantSearch >
-
+        </>
 )
 
 }
